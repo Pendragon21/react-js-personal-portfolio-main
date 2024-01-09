c9a65b26393c8d85766d948d02c00a1483bb9d21
Pendragon21 <rafaelbemvindo@hotmail.com>
2024-01-08 22:04:23 -0300

    "finished"

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,27 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/logo.jpeg" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Finishing a course in Computer Engineering at UFC with knowledge mainly in Front End and Data Science
+            development using the React and Python ecosystem technologies, I've already worked in a startup, an internship
+            and in a multinational company even before I graduated, I learned to solve problems in a practical
+            way even being very young. I am looking for opportunities to apply my knowledge, learn and
+            aggregate with my expertise
+ 
+          </p>
+          <p className="hero--section-description">
+            Already work at Dell and European companies, besides be a brazilian I know english in a good level, contact me 
+            and we talk more!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-scroll";
+
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hi, I'm Rafael</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Front end | Data Science</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            I'm a enthusiastic develepor, willing to show my skills and learn even more. 
+            <br /> Love cats, chess and read.
+          </p>
+        </div>
+        <br />
+        <a href="./img/CV-English.pdf" class="button" download className="btn btn-primary ">Dowload CV</a>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/logoR.jpeg" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
